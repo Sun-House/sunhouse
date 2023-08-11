4998824693cb109ad0cc5fe17c1b1c09a2591de0
AlexJSant <alex.jose7764@gmail.com>
2023-08-11 18:08:35 -0300

    Atualizado: montagem-pdp.js - TESTES

--- src/testes/js/montagem-pdp.js
+++ src/testes/js/montagem-pdp.js
@@ -11,15 +11,20 @@ function checarCep() {
     var cepNumerico = cep.replace(/\D/g, ''); // Remove não números do CEP
     cepNumerico = parseInt(cepNumerico); // Converte para número inteiro
 
+    const montagemBlock = document.getElementById("montagem-pdp");
+    const montagemInfo = document.getElementById("productMontagem-info");
+
     // Apenas debug: console.log("Valor do cepNumerico: " + cepNumerico);
 
     // Verifica se o CEP está nas faixas especificadas
     if ((cepNumerico >= 1000000 && cepNumerico <= 5999999) ||
         (cepNumerico >= 8000000 && cepNumerico <= 8499999)) {
-        document.getElementById("montagem-pdp").classList.add("montagem_show");
+        montagemBlock.classList.add("montagem_show");
+        montagemInfo.classList.add("montagem_show");
         // Apenas debug: console.log("Retorno: Verdadeiro (CEP dentro da faixa)");
     } else {
         // Apenas debug: console.log("Retorno: Falso (CEP fora da faixa)");
-        document.getElementById("montagem-pdp").classList.remove("montagem_show");
+        montagemBlock.classList.remove("montagem_show");
+        montagemInfo.classList.remove("montagem_show");
     }
 }
