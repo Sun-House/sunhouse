94bf8cd1c88c3b104e56221dc77a0808e2aa563a
AlexJSant <alex.jose7764@gmail.com>
2023-08-16 17:15:38 -0300

    Atualizado: sunhouse-product-bs.js

--- src/js/sunhouse-product-bs.js
+++ src/js/sunhouse-product-bs.js
@@ -209,3 +209,31 @@ function displayGratis() {
         }
     });
 }
+
+// Montagem Service Fn
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        document.getElementById("btnFreteSimulacao").addEventListener("click", function () {
+            checarCep();
+        });
+    }, 1000); // 1 segundos de espera
+});
+
+function checarCep() {
+    var cep = document.getElementById("txtCep").value;
+    var cepNumerico = cep.replace(/\D/g, ''); // Remove não números do CEP
+    cepNumerico = parseInt(cepNumerico); // Converte para número inteiro
+
+    const montagemBlock = document.getElementById("montagem-pdp");
+    const montagemInfo = document.getElementById("productMontagem-info");
+
+    // Verifica se o CEP está nas faixas especificadas
+    if ((cepNumerico >= 1000000 && cepNumerico <= 5999999) ||
+        (cepNumerico >= 8000000 && cepNumerico <= 8499999)) {
+        montagemBlock.classList.add("montagem_show");
+        montagemInfo.classList.add("montagem_show");
+    } else {
+        montagemBlock.classList.remove("montagem_show");
+        montagemInfo.classList.remove("montagem_show");
+    }
+}
