b8d6e75b3dd2b5ad9e939a815d910c5768c732cc
AlexJSant <alex.jose7764@gmail.com>
2023-08-21 13:23:43 -0300

    Criado: sunhouse-product-custom-bs.js Atualizados:     sunhouse-product-custom.js     sunhouse-measures.js

--- src/js/sunhouse-product-custom-bs.js
+++ src/js/sunhouse-product-custom-bs.js
@@ -10,6 +10,7 @@ function disableproduto() {
         $(".productContent__infos-freight").show()))
     }
 }
+
 function adicionarJSON_LD() {
     var scriptbox = document.createElement("script"),
     productname = document.querySelector("div.productContent__infos > div.productContent__infos-name > h1").textContent,
@@ -50,35 +51,12 @@ function adicionarJSON_LD() {
     }),
     document.querySelector("head").appendChild(scriptbox)
 }
-function fnInserirMedidas() {
-    var AltValue = skuJson.skus[0].measures.height
-      , LargValue = skuJson.skus[0].measures.width
-      , ProfValue = skuJson.skus[0].measures.length
-      , PesoValue = skuJson.skus[0].measures.weight;
-    AltValue <= 99 ? (AltValue = (AltValue = AltValue.toFixed(2)).replace(".", ","),
-    document.getElementById("alturaReal").innerHTML = " " + AltValue + " centÃ­metros") : AltValue >= 100 && AltValue <= 199 ? (AltValue = (AltValue = (AltValue /= 100).toString()).replace(".", ","),
-    document.getElementById("alturaReal").innerHTML = " " + AltValue + "0 metro") : (AltValue = (AltValue = (AltValue /= 100).toString()).replace(".", ","),
-    document.getElementById("alturaReal").innerHTML = " " + AltValue + "0 metros"),
-    LargValue <= 99 ? (LargValue = (LargValue = LargValue.toFixed(2)).replace(".", ","),
-    document.getElementById("larguraReal").innerHTML = " " + LargValue + " centÃ­etros") : LargValue >= 100 && LargValue <= 199 ? (LargValue = (LargValue = (LargValue /= 100).toString()).replace(".", ","),
-    document.getElementById("larguraReal").innerHTML = " " + LargValue + "0 metro") : (LargValue = (LargValue = (LargValue /= 100).toString()).replace(".", ","),
-    document.getElementById("larguraReal").innerHTML = " " + LargValue + "0 metros"),
-    ProfValue <= 99 ? (ProfValue = (ProfValue = ProfValue.toFixed(2)).replace(".", ","),
-    document.getElementById("profundidadeReal").innerHTML = " " + ProfValue + " centÃ­metros") : ProfValue >= 100 && ProfValue <= 199 ? (ProfValue = (ProfValue = (ProfValue /= 100).toString()).replace(".", ","),
-    document.getElementById("profundidadeReal").innerHTML = " " + ProfValue + "0 metro") : (ProfValue = (ProfValue = (ProfValue /= 100).toString()).replace(".", ","),
-    document.getElementById("profundidadeReal").innerHTML = " " + ProfValue + "0 metros"),
-    PesoValue <= 999 ? document.getElementById("pesoReal").innerHTML = " " + PesoValue + " gramas" : PesoValue >= 1e3 && PesoValue <= 1999 ? (PesoValue = (PesoValue = (PesoValue /= 1e3).toString()).replace(".", ","),
-    document.getElementById("pesoReal").innerHTML = " " + PesoValue + " kilograma") : (PesoValue = (PesoValue = (PesoValue /= 1e3).toString()).replace(".", ","),
-    document.getElementById("pesoReal").innerHTML = " " + PesoValue + " kilogramas")
-}
+
 $(document).ready((function() {
     alterar = new disableproduto,
-    alterar.montar(),
-    fnInserirMedidas(),
+    alterar.montar(),    
     setTimeout(adicionarJSON_LD, 3e3)
-}
-)),
-$(document).ready((function() {
+})), $(document).ready((function() {
     const CENTIMETER = "cm"
       , METER = "mt"
       , heightMeasureUnit = "CentÃ­metro" === $(".measures-height").text() ? "cm" : "mt"
@@ -90,5 +68,4 @@ $(document).ready((function() {
     $(".productContent__images-dimensions--height strong:last").text(alturaReal + heightMeasureUnit),
     $(".productContent__images-dimensions--width strong:last").text(larguraReal + widthMeasureUnit),
     $(".productContent__images-dimensions--length strong:last").text(comprimentoReal + lengthMeasureUnit)
-}
-));
+}));
--- src/js/sunhouse-product-measures.js
+++ src/js/sunhouse-product-measures.js
@@ -1,28 +1,27 @@
 // Cria medidas do table da descricao detalhada
-function fnInserirMedidasTable() {
-    var AltValue = skuJson.skus[0].measures.height,
-        LargValue = skuJson.skus[0].measures.width,
-        ProfValue = skuJson.skus[0].measures.length,
-        PesoValue = skuJson.skus[0].measures.weight;
-    AltValue <= 99 ? (AltValue = (AltValue = AltValue.toFixed(2)).replace(".", ","),
-            document.getElementById("alturaReal").innerHTML = " " + AltValue + " cent&iacute;metros") : AltValue >= 100 && AltValue <= 199 ? (AltValue = (AltValue = (AltValue /= 100).toString()).replace(".", ","),
-            document.getElementById("alturaReal").innerHTML = " " + AltValue + "0 metro") : (AltValue = (AltValue = (AltValue /= 100).toString()).replace(".", ","),
-            document.getElementById("alturaReal").innerHTML = " " + AltValue + "0 metros"),
-        LargValue <= 99 ? (LargValue = (LargValue = LargValue.toFixed(2)).replace(".", ","),
-            document.getElementById("larguraReal").innerHTML = " " + LargValue + " centíetros") : LargValue >= 100 && LargValue <= 199 ? (LargValue = (LargValue = (LargValue /= 100).toString()).replace(".", ","),
-            document.getElementById("larguraReal").innerHTML = " " + LargValue + "0 metro") : (LargValue = (LargValue = (LargValue /= 100).toString()).replace(".", ","),
-            document.getElementById("larguraReal").innerHTML = " " + LargValue + "0 metros"),
-        ProfValue <= 99 ? (ProfValue = (ProfValue = ProfValue.toFixed(2)).replace(".", ","),
-            document.getElementById("profundidadeReal").innerHTML = " " + ProfValue + " cent&iacute;metros") : ProfValue >= 100 && ProfValue <= 199 ? (ProfValue = (ProfValue = (ProfValue /= 100).toString()).replace(".", ","),
-            document.getElementById("profundidadeReal").innerHTML = " " + ProfValue + "0 metro") : (ProfValue = (ProfValue = (ProfValue /= 100).toString()).replace(".", ","),
-            document.getElementById("profundidadeReal").innerHTML = " " + ProfValue + "0 metros"),
-        PesoValue <= 999 ? document.getElementById("pesoReal").innerHTML = " " + PesoValue + " gramas" : PesoValue >= 1e3 && PesoValue <= 1999 ? (PesoValue = (PesoValue = (PesoValue /= 1e3).toString()).replace(".", ","),
-            document.getElementById("pesoReal").innerHTML = " " + PesoValue + " kilograma") : (PesoValue = (PesoValue = (PesoValue /= 1e3).toString()).replace(".", ","),
-            document.getElementById("pesoReal").innerHTML = " " + PesoValue + " kilogramas")
-}
+$(document).ready(function () {
+    // Valor elementos origem
+    const altToMove = document.querySelector(".lblAlturaReal").textContent + " " + document.querySelector(".measures-height").textContent;
+    const largToMove = document.querySelector(".lblLarguraReal").textContent + " " + document.querySelector(".measures-width").textContent;
+    const profToMove = document.querySelector(".lblComprimentoReal").textContent + " " + document.querySelector(".measures-length").textContent;
+    const pesoToMove = document.querySelector(".lblPesoReal").textContent + " " + document.querySelector(".measures-weight").textContent;
+
+    // Elementos destino
+    const destAlt = document.getElementById("alturaReal");
+    const destLarg = document.getElementById("larguraReal");
+    const destProf = document.getElementById("profundidadeReal");
+    const destPeso = document.getElementById("pesoReal");
+
+    // Copiando os valores
+    destAlt.textContent = altToMove + "s";
+    destLarg.textContent = largToMove + "s";
+    destProf.textContent = profToMove + "s";
+    destPeso.textContent = pesoToMove + "s";
+});
 
 // Cria medidas do block abaixo da foto do produto
-function fnInserirMedidasBlocos() {
+//function fnInserirMedidasBlocos() {
+$(document).ready(function () {
     var AlturValue = skuJson.skus[0].measures.height,
         LargurValue = skuJson.skus[0].measures.width,
         ProfunValue = skuJson.skus[0].measures.length,
@@ -37,7 +36,8 @@ function fnInserirMedidasBlocos() {
     largBloco.innerHTML = LargurValue;
     profBloco.innerHTML = ProfunValue;
     pesoBloco.innerHTML = PesValue;
-}
+});
+//}
 
 // Faz os devidos ajustes nos displays de siglas e valores corretos no Block inicial de medidas PDP
 $(document).ready((function () {
@@ -56,6 +56,5 @@ $(document).ready((function () {
 
 // Executa funcoes blocadas acima
 $(document).ready(function () {
-    fnInserirMedidasTable(),
     fnInserirMedidasBlocos()
 });
