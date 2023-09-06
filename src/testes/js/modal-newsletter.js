a6f67777a110c1c8f716a5a42b2cf8373c38eb12
AlexJSant <alex.jose7764@gmail.com>
2023-09-06 18:13:26 -0300

    Atualizado: modal-newsletter.js

--- src/testes/js/modal-newsletter.js
+++ src/testes/js/modal-newsletter.js
@@ -4,24 +4,27 @@
 var modalShownToday = localStorage.getItem("modalShownToday");
 if (!modalShownToday) {
     // Monitora o evento de rolagem da página
-    window.addEventListener('scroll', function () {
+    function scrollHandler() {
         // Verifica a posição de rolagem
         if (window.scrollY >= 200) {
             // Abre o modal quando a posição de rolagem atinge 200 pixels
             $('#modal-news').modal('show');
+            // Remove o ouvinte de evento de rolagem após a primeira exibição do modal
+            window.removeEventListener('scroll', scrollHandler);
         }
-    });
+    }
+    window.addEventListener('scroll', scrollHandler);
 
     // Define uma variável no localStorage para rastrear que o modal já foi exibido hoje
     localStorage.setItem("modalShownToday", "true");
 }
 
-// Fecha o modal quando o usuário clica fora dele ou no botão de fechar
-$('#modal-news').on('hidden.bs.modal', function () {
-    $('#modal-news').modal('hide');
-});
+// Fecha o modal quando o usuário clica fora dele ou no botão de fechar - ACREDITO QUE JA TENHA NATIVO
+//$('#modal-news').on('hidden.bs.modal', function () {
+    //$('#modal-news').modal('hide');
+//});
 
-// Fecha o modal quando o usuário clica no botão de fechar
-$('#modalCloseButton').click(function () {
-    $('#modal-news').modal('hide');
-});
+// Fecha o modal quando o usuário clica no botão de fechar - ACREDITO QUE JA TENHA NATIVO
+//$('#modalCloseButton').click(function () {
+    //$('#modal-news').modal('hide');
+//});
