0b96f747d588cdfab85feacec2feeeeb73f5db76
AlexJSant <alex.jose7764@gmail.com>
2023-10-24 14:29:12 -0300

    Atualizado: sunhouse-black-theme.js

--- src/js/sunhouse-black-theme.js
+++ src/js/sunhouse-black-theme.js
@@ -1,13 +1,14 @@
 // Ativar somente em epoca de Black
 document.addEventListener("DOMContentLoaded", function () {
-    //setTimeout (function() {
-        const cookieExists = document.cookie.includes("cDark=darktheme");
+    //const cookieExists = document.cookie.includes("cDark=darktheme");
+    const bcookieExists = document.cookie.includes("bDark=blacktheme");
 
-        if (cookieExists === true) {
-            alert("If: ja tem o cookie");
-        } else {
-            criarCookie();
-            alert("Else: cookie criado agora");
-        }
-    //}, 2000)
+    //document.cookie = "bDark=blacktheme; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";    
+
+    if (bcookieExists === false) {
+        //document.cookie = "cDark=darktheme; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+        criarCookie();
+    } else {
+        document.cookie = "cDark=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    }
 });
