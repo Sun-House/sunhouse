80b19a904554c742dbfa92eb7dcba22a3e9cd8cd
AlexJSant <alex.jose7764@gmail.com>
2024-01-31 10:34:03 -0300

    Atualizado: testes/js/marketplace-brand.js

--- src/testes/js/marketplace-brand.js
+++ src/testes/js/marketplace-brand.js
@@ -6,9 +6,13 @@ setTimeout(function () {
 
     const brandLinked = document.getElementById("product_brand_link");
 
+    function removerAcentos(str) {
+        return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    }
+
     if (itemComDados && itemComDados.productBrandName && !(itemComDados.productBrandName == "Sun House")) {
         var brandName = itemComDados.productBrandName;
-        var formattedBrandName = brandName.replace(/\s+/g, '-');
+        var formattedBrandName = removerAcentos(brandName).replace(/\s+/g, '-');
 
         brandLinked.href = "https://www.sunhouse.com.br/lojista/" + formattedBrandName;
         brandLinked.textContent = brandName;
