bdb60d9bd759e2ca6aa259daba1ea3d8e0775b28
AlexJSant <alex.jose7764@gmail.com>
2024-02-05 14:39:27 -0300

    Ajustando...: sunhouse-lojista-bs.js Atualizado: sunhouse-common-bs-newTheme.css

--- src/js/sunhouse-lojista-bs.js
+++ src/js/sunhouse-lojista-bs.js
@@ -1,3 +1,30 @@
+//Obtem final da URL com Marca alvo
+function obterConteudoFinalDaURL() {
+    // URL da sua string
+    var url = window.location.href;
+
+    // Remover os parâmetros de consulta
+    var urlSemQuery = url.split('?')[0];
+
+    // Dividir a URL pela barra "/"
+    var partesDaURL = urlSemQuery.split("/");
+
+    // Obter o último elemento do array resultante
+    var conteudoFinal = partesDaURL.pop();
+
+    // Armazenar a variável para uso posterior
+    window.conteudoFinalDaURL = conteudoFinal;
+
+    // Imprimir no console para verificar
+    console.log("Conteúdo Final da URL:", conteudoFinal);
+}
+
+// Chamar a função quando a página carregar
+window.onload = obterConteudoFinalDaURL;
+
+//
+
+// Redireciona para cada Categoria + Marca alvo
 function redirecionaCategBrand() {
     var selectElement = document.getElementById("select_qf");
     var selectedValue = selectElement.value;
