b60a9b49745af39d74c1a995b8dbcb8cb3403099
AlexJSant <alex.jose7764@gmail.com>
2024-02-05 12:25:47 -0300

    Atualizado: sunhouse-product-bs.js

--- src/js/sunhouse-product-bs.js
+++ src/js/sunhouse-product-bs.js
@@ -260,3 +260,31 @@ window.addEventListener("load", function () {
         }
     }, 1000); // 1 segundo de espera
 });
+
+// Mostra info de Marca e Loja do produto - Marketplace
+//window.addEventListener("load", function () {
+    setTimeout(function () {
+        var itemComDados = dataLayer.find(function (item) {
+            return item.productBrandName;
+        });
+
+        const brandLinked = document.getElementById("product_brand_link");
+
+        function removerAcentos(str) {
+            return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+        }
+
+        if (itemComDados && itemComDados.productBrandName && !(itemComDados.productBrandName == "Sun House")) {
+            var brandName = itemComDados.productBrandName;
+            var formattedBrandName = removerAcentos(brandName).replace(/\s+/g, '-');
+
+            brandLinked.href = "https://www.sunhouse.com.br/lojista/" + formattedBrandName;
+            brandLinked.textContent = brandName;
+        } else {
+            var brandNotLinked = document.getElementById("brand_sunhouse_notlinked");
+
+            brandNotLinked.style.display = 'block';
+            brandLinked.style.display = 'none';
+        }
+    }, 2000);
+//});
