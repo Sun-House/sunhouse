9cd85892e3e7ba7d1f4ecca31010b54b011c4302
AlexJSant <alex.jose7764@gmail.com>
2024-03-08 15:12:22 -0300

    Criado - Testes: valida-login.js

---
+++ src/testes/js/valida-login.js
@@ -0,0 +1,40 @@
+// Ajustar abaixo para mostrar apenas uma vez - via cookie - e sumir depois do primeiro login - por sessão no caso
+
+// Valida se o usuario esta logado, se ESTIVER, ele faz algo
+// nesse bloco do if, colocar a acao de trocar os elementos html que se referem ao usuario logado e ao usuario deslogado
+document.addEventListener('DOMContentLoaded', function () {
+    userLoginArea();
+});
+
+function userLoginArea() {
+    if (typeof dataLayer !== 'undefined') {
+
+        // Itera sobre as entradas no dataLayer
+        for (var i = 0; i < dataLayer.length; i++) {
+
+            // Verifica se a entrada "visitorLoginState" existe e possui o valor true
+            if (dataLayer[i].visitorLoginState === true) {
+
+                // Faça algo se a condição for atendida
+                console.log("TRUE. Esta logado");
+
+                // Inicialize o popover no botão
+                $('#logged_in').popover();
+
+                // Mostre o popover
+                $('#logged_in').popover('show');
+
+                // Oculte o popover após 5 segundos
+                setTimeout(function () {
+                    $('#logged_in').popover('hide');
+                }, 5000);
+
+                // Se desejar interromper a iteração após encontrar a primeira ocorrência,
+                // você pode usar o break
+                break;
+            } else { // else apenas para teste e depuracao
+                console.log('FALSE. Não esta logado');
+            }
+        }
+    }
+}
