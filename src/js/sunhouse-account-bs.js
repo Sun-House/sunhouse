0239e4810a92276ec14a0ea2d56d903e49bf6f56
AlexJSant <alex.jose7764@gmail.com>
2024-03-11 12:08:02 -0300

    Criado: sunhouse-account-bs.js Alterado: sunhouse-custom-bs.js

--- src/js/sunhouse-account-bs.js
+++ src/js/sunhouse-account-bs.js
@@ -1,1 +1,16 @@
+// Verifica se esta logado
 // Codigo de validação de Login aqui
+
+
+// Botao para Sair-Deslogar
+function customLogout() {
+    $.ajax({
+        url: "/no-cache/user/logout"
+    }).done(function () {
+        // Aqui você pode adicionar qualquer lógica personalizada que deseja executar após a conclusão da requisição AJAX
+        console.log("Logout realizado com sucesso!");
+
+        // Agora, redirecione para a página atual
+        window.location.reload(); // Isso recarrega a página atual, mantendo-a dinâmica
+    });
+}
--- src/js/sunhouse-custom-bs.js
+++ src/js/sunhouse-custom-bs.js
@@ -1,5 +1,20 @@
 // Inicia Popper Tooltips
+//$(document).ready(function () {
+    //const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    //const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+//});
+
+// Inicia Popper - Tooltips e Popovers
 $(document).ready(function () {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    const triggerList = document.querySelectorAll('[data-bs-toggle="tooltip"], [data-bs-toggle="popover"]');
+
+    if (triggerList.length > 0) {
+        const tooltipAndPopoverList = [...triggerList].map(triggerEl => {
+            if (triggerEl.getAttribute('data-bs-toggle') === 'tooltip') {
+                return new bootstrap.Tooltip(triggerEl);
+            } else if (triggerEl.getAttribute('data-bs-toggle') === 'popover') {
+                return new bootstrap.Popover(triggerEl);
+            }
+        });
+    }
 });
