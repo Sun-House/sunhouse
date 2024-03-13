465a79f464783c9ff00d0c7c53dca27bc4c11f7f
AlexJSant <alex.jose7764@gmail.com>
2024-03-13 16:07:40 -0300

    Atualizado: sunhouse-product-bs.js

--- src/js/sunhouse-product-bs.js
+++ src/js/sunhouse-product-bs.js
@@ -356,6 +356,39 @@ $(document).ready(function () {
     }
 });
 
+// Move os elementos das abas de Descricao do Produto - Desk e Mobile
+function moveElementos() {
+    const prodDesc = document.getElementById('product-description');
+    const prodFicha = document.getElementById('product-ficha');
+    const prodRate = document.getElementById('product-rate');
+
+    if (window.matchMedia('(max-width: 1199px)').matches) {
+        // Verifica se a largura da tela é no máximo 1199px
+        // Move os elementos para um novo local no DOM
+        const prodDescMob = document.getElementById('mobile_product-description');
+        const prodFichaMob = document.getElementById('mobile_product-ficha');
+        const prodRateMob = document.getElementById('mobile_product-rate');
+
+        prodDescMob.appendChild(prodDesc);
+        prodFichaMob.appendChild(prodFicha);
+        prodRateMob.appendChild(prodRate);
+    } else {
+        // Se a largura da tela for maior que 1199px, restaura a posição original dos elementos
+        const prodDescDesk = document.getElementById('descricao-produto');
+        const prodFichaDesk = document.getElementById('ficha-produto');
+        const prodRateDesk = document.getElementById('rate-produto');
+
+        prodDescDesk.appendChild(prodDesc);
+        prodFichaDesk.appendChild(prodFicha);
+        prodRateDesk.appendChild(prodRate);
+    }
+}
+
+// Chama o Bloco acima quando a janela é carregada e quando sua largura é alterada
+window.addEventListener('load', moveElementos);
+window.addEventListener('resize', moveElementos);
+
+
 // Reposicionamento Mobile Outras Cores
 function moveOutrasCores() {
     if ($(window).width() <= 1199) {
