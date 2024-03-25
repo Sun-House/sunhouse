956c0959d0a6d670438b79d32631661bcec03767
AlexJSant <alex.jose7764@gmail.com>
2024-03-25 11:54:25 -0300

    Atualizados:     sunhouse-checkout6-common-new.css     /testes/js/sunhouse-shelf-buynow.js

--- src/testes/js/sunhouse-shelf-buynow.js
+++ src/testes/js/sunhouse-shelf-buynow.js
@@ -1,38 +1,13 @@
-// NAO ESTA FUNCIONANDO
-function fnBuynow() {
-    $("body").on("click", "#buy_now", (function (a) {
-        a.preventDefault();
-        var b = $(this).parents(".shelfProduct").attr("data-id");
-        $.ajax({
-            url: "/api/catalog_system/pub/products/search/?fq=productId:" + b,
-            method: "GET",
-            timeout: 0,
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            }
-        }).done((function (a) {
-            var b = {
-                id: a[0].items[0].itemId,
-                quantity: 1,
-                seller: "1"
-            };
-
-            window.location.href = '/checkout';
-        }))
-    }))
-}
-
-
-
-// 21.03.2024
+// 25.03.2024 - Funcional
 // Função para adicionar o ouvinte de evento de clique a um elemento
-function adicionarOuvinteClique(idElemento, funcaoCallback) {
-    var elemento = document.getElementById(idElemento);
-    if (elemento) {
-        elemento.addEventListener('click', funcaoCallback);
+function adicionarOuvinteClique(seletor, funcaoCallback) {
+    var elementos = document.querySelectorAll(seletor);
+    if (elementos.length > 0) {
+        elementos.forEach(function (elemento) {
+            elemento.addEventListener('click', funcaoCallback);
+        });
     } else {
-        console.error('Elemento não encontrado com o ID:', idElemento);
+        console.error('Nenhum elemento encontrado com o seletor:', seletor);
     }
 }
 
@@ -57,57 +32,11 @@ function cliqueNoBotao(a) {
             seller: "1"
         };
 
-        //window.location.href = '/checkout';
-        alert('Cliquei no botão!' + skuCode);
+        vtexjs.checkout.addToCart([skuCode], null, 1)
+
+        window.location.href = '/checkout';
     }))
 }
 
 // Chamando a função para adicionar o ouvinte de evento de clique ao botão
-adicionarOuvinteClique('buy_now', cliqueNoBotao);
-
-
-
-
-//USAR ESSE COMO BASE PQ FUNCIONA NO CART:
-"use strict";
-
-function fnAddtoCart() {
-    $("body").on("click", ".add-to-cart", (function (a) {
-        a.preventDefault();
-        var b = $(this).parents(".shelfProduct").attr("data-id");
-        $.ajax({
-            url: "/api/catalog_system/pub/products/search/?fq=productId:" + b,
-            method: "GET",
-            timeout: 0,
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            }
-        }).done((function (a) {
-            var b = {
-                id: a[0].items[0].itemId,
-                quantity: 1,
-                seller: "1"
-            };
-            vtexjs.checkout.addToCart([b], null, 1).done((function (a) {
-                toastr.options = {
-                    closeButton: !0,
-                    debug: !1,
-                    newestOnTop: !1,
-                    progressBar: !0,
-                    positionClass: "toast-top-center",
-                    preventDuplicates: !1,
-                    onclick: null,
-                    showDuration: "500",
-                    hideDuration: "1000",
-                    timeOut: "5000",
-                    extendedTimeOut: "0",
-                    showEasing: "swing",
-                    hideEasing: "linear",
-                    showMethod: "fadeIn",
-                    hideMethod: "fadeOut"
-                }, toastr.success("Produto adicionado ao carrinho!")
-            }))
-        }))
-    }))
-}
+adicionarOuvinteClique('#buy_now', cliqueNoBotao);
