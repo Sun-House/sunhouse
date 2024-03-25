8342b79f4cc322b279a42d53e001f0641e11cfda
AlexJSant <alex.jose7764@gmail.com>
2024-03-25 16:24:10 -0300

    Atualizado: sunhouse-catalog-bs.js Renomeado: sunhouse-shelf-buynow.js -> catalog-shelf-buynow.js

--- src/js/sunhouse-catalog-bs.js
+++ src/js/sunhouse-catalog-bs.js
@@ -135,3 +135,43 @@ $(document).ready(function () {
     window.addEventListener("popstate", carregarPopperURL);
     window.addEventListener("hashchange", carregarPopperURL);
 });
+
+// Cria direcionamento direto ao cart para quando o botão de 'Comprar' é clicado no grid de produtos
+function adicionarOuvinteClique(seletor, funcaoCallback) {
+    var elementos = document.querySelectorAll(seletor);
+    if (elementos.length > 0) {
+        elementos.forEach(function (elemento) {
+            elemento.addEventListener('click', funcaoCallback);
+        });
+    } else {
+        console.error('Nenhum elemento encontrado com o seletor:', seletor);
+    }
+}
+
+// Função de callback para o clique no botão
+function cliqueNoBotao(a) {
+    a.preventDefault();
+    var skuCode = $(this).closest("li.shelfProduct").attr("data-id");
+
+    $.ajax({
+        url: "/api/catalog_system/pub/products/search/?fq=productId:" + skuCode,
+        method: "GET",
+        timeout: 0,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        }
+    }).done((function (a) {
+        var skuCode = {
+            id: a[0].items[0].itemId,
+            quantity: 1,
+            seller: "1"
+        };
+
+        vtexjs.checkout.addToCart([skuCode], null, 1)
+
+        window.location.href = '/checkout';
+    }))
+}
+
+adicionarOuvinteClique('#buy_now', cliqueNoBotao);
