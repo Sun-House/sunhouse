856fcf17a02bd04e80c9d01380de892a6a80bd50
AlexJSant <alex.jose7764@gmail.com>
2024-04-01 17:40:20 -0300

    Atualizado: sunhouse-home.js

--- src/js/sunhouse-home.js
+++ src/js/sunhouse-home.js
@@ -1,5 +1,4 @@
-//"use strict";
-
+"use strict";
 $(document).ready(function () {
     if (window.innerWidth < 1200) {
         $(".highlight, #banner_mobile").slick({
@@ -14,26 +13,6 @@ $(document).ready(function () {
             arrows: false,
             autoplay: false
         });
-
-        // Valida e cria novas prateleiras SmartHint
-        //setTimeout(function () {
-            //var prateleira = $('.slick-it');
-            //var prateleiraSM = $('.shelf.n12colunas');
-            
-            //if (prateleiraSM.hasClass('slick-initialized')) {
-                //prateleiraSM.slick('unslick');
-            //}
-            
-            //prateleiraSM.slick({
-                //slidesToShow: 2,
-                //slidesToScroll: 2,
-                //dots: true,
-                //arrows: true,
-                //prevArrow: "<i id='prev_arrow__custom' class='fa-solid fa-circle-chevron-left arrow-custom'></i>",
-                //nextArrow: "<i id='next_arrow__custom' class='fa-solid fa-circle-chevron-right arrow-custom'></i>",
-                //autoplay: false
-            //}), console.log('teste 1 SM: realizado');
-        //}, 3000);
     } else {
         $(".highlight").slick({
             dots: true,
@@ -48,7 +27,7 @@ $(document).ready(function () {
             autoplay: false
         });
 
-        // SmartHint ??
+        // SmartHint ?? - VER
         //$(".bestSellers .shelf ul, .shelfNews .shelf ul").slick({
             //slidesToShow: 4,
             //slidesToScroll: 4,
