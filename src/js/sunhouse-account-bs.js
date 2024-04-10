446dd37dcbe9f463eaf923717c3d0d5822be5bf6
AlexJSant <alex.jose7764@gmail.com>
2024-04-10 11:35:10 -0300

    Atualizado: sunhouse-account-bs.js

--- src/js/sunhouse-account-bs.js
+++ src/js/sunhouse-account-bs.js
@@ -28,14 +28,45 @@ function userLoginArea() {
     
                 // Seleciona todos os elementos com a Classe "logged-in" e executa o popover em cada um
                 $('.logged-in').each(function() {
-                    $(this).popover('show');
+                    //$(this).popover('show');
                     
-                    var popElements = $(this);
+                    //var popElements = $(this);
+                    //setTimeout(function() {
+                        //popElements.popover('hide');
+                    //}, 3000);
+
+                    var popElement = $(this);
+
+                    popElement.popover('show');
+                    
                     setTimeout(function() {
-                        popElements.popover('hide');
+                        popElement.popover('hide');
+                        popElement.popover('destroy');
                     }, 3000);
+
+                    document.querySelector('#wrap-topbar a').style.display = 'none';
+
+                    moveLoggedBlock();
                 });
             }
+
+            // Reposiciona Elementos
+            function moveLoggedBlock() {
+                if ($(window).width() <= 1199) {
+                    $("#login__loggedIn").appendTo("#mobile-login__loggedIn");
+                } else {
+                    // Volte o conteúdo para o local original se a largura for maior que 1199px
+                    $("#login__loggedIn").appendTo("#desk-login__loggedIn");
+                }
+            }
+
+            $(document).ready(function () {
+                moveLoggedBlock();
+            });
+
+            $(window).resize(function () {
+                moveLoggedBlock();
+            });
         }
     }, 2000);
 }
