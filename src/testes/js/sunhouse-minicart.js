6550f00852fad4bc403b20d5debf2c464b68f214
AlexJSant <alex.jose7764@gmail.com>
2024-04-24 13:08:09 -0300

    Criado: sunhouse-minicart.js

---
+++ src/testes/js/sunhouse-minicart.js
@@ -0,0 +1,17 @@
+// Em processo de DEV
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecione o elemento que deseja mover
+    var nativeMiniCartv2 = document.querySelector('.v2-vtexsc-cart .vtexsc-center');
+
+    // Selecione o elemento de destino
+    var sideBarMiniCart = document.getElementById('mini-cart__block');
+
+    // Verifique se ambos os elementos existem
+    if (nativeMiniCartv2 && sideBarMiniCart) {
+        // Mova o elemento para dentro do elemento de destino
+        sideBarMiniCart.appendChild(nativeMiniCartv2);
+    } else {
+        console.log('Um dos elementos não foi encontrado.');
+    }
+});
