d01c488a39498c565efce2e07db0ff5f2ca72db4
AlexJSant <alex.jose7764@gmail.com>
2024-05-03 12:11:40 -0300

    Atualizado: wishlist-test.js

--- src/testes/js/wishlist-test.js
+++ src/testes/js/wishlist-test.js
@@ -4,86 +4,57 @@
 // EM TESTES
 // Função para executar o código quando ocorrer uma alteração na estrutura do elemento #giftlistproduct
 function observeChanges() {
-    // Selecione o elemento alvo
-    //var target = document.getElementById('giftlistproduct');
     var target = document.querySelector('.giftlistproductsv2');
-
-    // Crie uma instância de MutationObserver e passe uma função de callback
     var observer = new MutationObserver(function(mutations) {
-        // Para cada mutação observada, execute o código
         mutations.forEach(function(mutation) {
-            // Verifique se a mutação é relacionada à adição de nós (elementos) à árvore DOM
             if (mutation.type === 'childList') {
-                // Execute o código original
                 executeCode();
             }
         });
     });
-
-    // Configure as opções de observação - observe mudanças na estrutura da árvore DOM
     var config = { childList: true, subtree: true };
-
-    // Inicie a observação do elemento alvo com as opções de configuração
     observer.observe(target, config);
 }
 
-// Função para executar o código original
 function executeCode() {
-    //
+    setTimeout(function () {
+        var productTrInput = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"] .inputedit');
+        
+        productTrInput.forEach(function (element) {
+            var button = document.createElement("button");
+            button.setAttribute("class", "wishlist--add-to-cart-button");
+            button.setAttribute("id", "wishlist__add-to-cart-button");
+
+            var link = document.createElement("a");
+            link.setAttribute("target", "_self");
+            link.setAttribute("class", "wishlist-add-to-cart");
+            link.setAttribute("id", "wishlist__add-to-cart");
+            link.setAttribute("href", "javascript:void(0)");
+            link.setAttribute("style", "display: block");
+            link.textContent = "Adicionar ao Carrinho";
+
+            button.appendChild(link);
+
+            element.parentNode.insertBefore(button, element.nextSibling);
+        });
+
+        var productTr = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"]');
         setTimeout(function () {
-            // Selecionar todos os elementos <tr> com o atributo data-bind="foreach: $parent.columns"
-            var productTrInput = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"] .inputedit');
-            
-            // Para cada elemento <tr> encontrado - cria botao
-            productTrInput.forEach(function (element) {
-                // Criar o elemento <button>
-                var button = document.createElement("button");
-
-                // Adicionar o evento de clique ao botão
-                button.setAttribute("class", "wishlist--add-to-cart-button");
-                button.setAttribute("id", "wishlist__add-to-cart-button");
-
-                // Criar o elemento <a> dentro do botão
-                var link = document.createElement("a");
-
-                // Adicionar os atributos ao elemento <a>
-                link.setAttribute("target", "_self");
-                link.setAttribute("class", "wishlist-add-to-cart");
-                link.setAttribute("id", "wishlist__add-to-cart");
-                link.setAttribute("href", "javascript:void(0)");
-                link.setAttribute("style", "display: block");
-                link.textContent = "Adicionar ao Carrinho"; // Adicionar o texto dentro do elemento <a>
-
-                // Adicionar o elemento <a> dentro do botão
-                button.appendChild(link);
-
-                // Adicionar o botão após o elemento <tr>
-                element.parentNode.insertBefore(button, element.nextSibling);
-            });
-
-            // Atribui o ouvinte de function a cada botao criado
-            var productTr = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"]');
-            setTimeout(function () {
-                // Para cada elemento <tr> encontrado - aciona botao
-                productTr.forEach(function (element) {
-                    // Adiciona um evento de clique para cada botão de adicionar ao carrinho
-                    var addToCartButton = element.querySelector('.wishlist--add-to-cart-button');
-                    addToCartButton.addEventListener('click', function () {
-                        // Encontra o input associado a este botão
-                        var input = element.querySelector('.giftlistsku-input-wishedamt');
-                        // Obtém o valor do atributo "name" do input
-                        var skuId = input.getAttribute('name');
-                        // Chama a função que adiciona ao carrinho, passando o skuId
-                        addtoCartWishlist(skuId);
-                    });
+            productTr.forEach(function (element) {
+                var addToCartButton = element.querySelector('.wishlist--add-to-cart-button');
+                addToCartButton.addEventListener('click', function () {
+                    var input = element.querySelector('.giftlistsku-input-wishedamt');
+                    var skuId = input.getAttribute('name');
+                    addtoCartWishlist(skuId);
                 });
-            }, 2000);
+            });
         }, 2000);
-    //
-    window.onload(); // Chamada da função onload para garantir que o código seja executado
+    }, 2000);
 }
 
-// Chame a função para observar as alterações na estrutura do elemento #giftlistproduct
+// Chame a função executeCode diretamente para garantir que o código seja executado no carregamento inicial
+executeCode();
+// Chame a função para observar as alterações na estrutura do elemento .giftlistproductsv2
 observeChanges();
 // EM TESTES
 
