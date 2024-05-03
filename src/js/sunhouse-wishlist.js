828ce0048f0b2e4f7d83bed79d26b6c4b503a4b3
AlexJSant <alex.jose7764@gmail.com>
2024-05-03 10:37:33 -0300

    Atualizado: sunhouse-wishlist-bs.css

--- src/js/sunhouse-wishlist.js
+++ src/js/sunhouse-wishlist.js
@@ -1,56 +1,58 @@
 // NAO ESTA FUNCIONANDO - abaixo
 // Cria o botao de Adicionar ao Carrinho
-$(document).ready(function () {
-//function functionWrap() {
-    // Selecionar todos os elementos <tr> com o atributo data-bind="foreach: $parent.columns"
-    var productTrInput = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"] .inputedit');
-    
-    // Para cada elemento <tr> encontrado - cria botao
-    productTrInput.forEach(function (element) {
-        // Criar o elemento <button>
-        var button = document.createElement("button");
-
-        // Adicionar o evento de clique ao botão
-        button.setAttribute("class", "wishlist--add-to-cart-button");
-        button.setAttribute("id", "wishlist__add-to-cart-button");
-
-        // Criar o elemento <a> dentro do botão
-        var link = document.createElement("a");
-
-        // Adicionar os atributos ao elemento <a>
-        link.setAttribute("target", "_self");
-        link.setAttribute("class", "wishlist-add-to-cart");
-        link.setAttribute("id", "wishlist__add-to-cart");
-        link.setAttribute("href", "javascript:void(0)");
-        link.setAttribute("style", "display: block");
-        link.textContent = "Adicionar ao Carrinho"; // Adicionar o texto dentro do elemento <a>
-
-        // Adicionar o elemento <a> dentro do botão
-        button.appendChild(link);
-
-        // Adicionar o botão após o elemento <tr>
-        element.parentNode.insertBefore(button, element.nextSibling);
-    });
-
-    // Atribui o ouvinte de function a cada botao criado
-    var productTr = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"]');
+window.onload = function() {
     setTimeout(function () {
-        // Para cada elemento <tr> encontrado - aciona botao
-        productTr.forEach(function (element) {
-            // Adiciona um evento de clique para cada botão de adicionar ao carrinho
-            var addToCartButton = element.querySelector('.wishlist--add-to-cart-button');
-            addToCartButton.addEventListener('click', function () {
-                // Encontra o input associado a este botão
-                var input = element.querySelector('.giftlistsku-input-wishedamt');
-                // Obtém o valor do atributo "name" do input
-                var skuId = input.getAttribute('name');
-                // Chama a função que adiciona ao carrinho, passando o skuId
-                addtoCartWishlist(skuId);
-            });
+
+        // Selecionar todos os elementos <tr> com o atributo data-bind="foreach: $parent.columns"
+        var productTrInput = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"] .inputedit');
+        
+        // Para cada elemento <tr> encontrado - cria botao
+        productTrInput.forEach(function (element) {
+            // Criar o elemento <button>
+            var button = document.createElement("button");
+
+            // Adicionar o evento de clique ao botão
+            button.setAttribute("class", "wishlist--add-to-cart-button");
+            button.setAttribute("id", "wishlist__add-to-cart-button");
+
+            // Criar o elemento <a> dentro do botão
+            var link = document.createElement("a");
+
+            // Adicionar os atributos ao elemento <a>
+            link.setAttribute("target", "_self");
+            link.setAttribute("class", "wishlist-add-to-cart");
+            link.setAttribute("id", "wishlist__add-to-cart");
+            link.setAttribute("href", "javascript:void(0)");
+            link.setAttribute("style", "display: block");
+            link.textContent = "Adicionar ao Carrinho"; // Adicionar o texto dentro do elemento <a>
+
+            // Adicionar o elemento <a> dentro do botão
+            button.appendChild(link);
+
+            // Adicionar o botão após o elemento <tr>
+            element.parentNode.insertBefore(button, element.nextSibling);
         });
-    }, 2000);
-//}
-});
+
+        // Atribui o ouvinte de function a cada botao criado
+        var productTr = document.querySelectorAll('tr[data-bind="foreach: $parent.columns"]');
+        setTimeout(function () {
+            // Para cada elemento <tr> encontrado - aciona botao
+            productTr.forEach(function (element) {
+                // Adiciona um evento de clique para cada botão de adicionar ao carrinho
+                var addToCartButton = element.querySelector('.wishlist--add-to-cart-button');
+                addToCartButton.addEventListener('click', function () {
+                    // Encontra o input associado a este botão
+                    var input = element.querySelector('.giftlistsku-input-wishedamt');
+                    // Obtém o valor do atributo "name" do input
+                    var skuId = input.getAttribute('name');
+                    // Chama a função que adiciona ao carrinho, passando o skuId
+                    addtoCartWishlist(skuId);
+                });
+            });
+        }, 2000);
+
+    }, 3000);
+};
 // NAO ESTA FUNCIONANDO - acima
 
 // Adiciona ao Carrinho v2 - Native Vtex
