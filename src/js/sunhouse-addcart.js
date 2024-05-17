c8e3b3ec77cc2b225c6e9ed2824fc170c475af62
AlexJSant <alex.jose7764@gmail.com>
2024-05-17 14:55:23 -0300

    Atualizado: sunhouse-addcart.js

--- src/js/sunhouse-addcart.js
+++ src/js/sunhouse-addcart.js
@@ -1,5 +1,6 @@
+// Teste:
 function fnAddtoCart() {
-    $("body").on("click", ".add-to-cart", (function (a) {
+    //$("body").on("click", ".add-to-cart", (function (a) {
         a.preventDefault();
         var b = $(this).parents(".shelfProduct").attr("data-id");
         $.ajax({
@@ -11,12 +12,30 @@ function fnAddtoCart() {
                 "Content-Type": "application/json"
             }
         }).done((function (a) {
-            var b = {
-                id: a[0].items[0].itemId,
-                quantity: 1,
-                seller: "1"
-            };
-            vtexjs.checkout.addToCart([b], null, 1).done((function (a) {
+            if (a) {
+                var b = {
+                    id: a[0].items[0].itemId,
+                    quantity: 1,
+                    seller: "1"
+                };
+                vtexjs.checkout.addToCart([b], null, 1).done((function (a) {
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: "top",
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.onmouseenter = Swal.stopTimer;
+                            toast.onmouseleave = Swal.resumeTimer;
+                        }
+                    });
+                    Toast.fire({
+                        icon: "success",
+                        title: "Produto adicionado ao carrinho!"
+                    });
+                }));
+            } else {
                 const Toast = Swal.mixin({
                     toast: true,
                     position: "top",
@@ -29,10 +48,10 @@ function fnAddtoCart() {
                     }
                 });
                 Toast.fire({
-                    icon: "success",
-                    title: "Produto adicionado ao carrinho!"
+                    icon: "error",
+                    title: "Produto está indisponível"
                 });
-            }))
+            }
         }))
-    }))
+    //}))
 }
