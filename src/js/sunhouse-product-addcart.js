5b176119d9ad46d500a0559fbc514b8134a1679e
AlexJSant <alex.jose7764@gmail.com>
2024-07-12 16:52:48 -0300

    Atualizados:     sunhouse-product-addcart.js     sunhouse-product-custom-bs.js

--- src/js/sunhouse-product-addcart.js
+++ src/js/sunhouse-product-addcart.js
@@ -1,29 +1,3 @@
-// Usar para addcart pdp
-
-//function fnPdpAddtoCart() {
-    //$("body").on("click", "#addCart_main", (function (a) {
-        //a.preventDefault();
-        //var b = $(this).parents(".shelfProduct").attr("data-id");
-        //var b = document.getElementById('codigo_produto').innerHTML;
-        //$.ajax({
-            //url: "/api/catalog_system/pub/products/search/?fq=productId:" + b,
-            //method: "GET",
-            //timeout: 0,
-            //headers: {
-                //Accept: "application/json",
-                //"Content-Type": "application/json"
-            //}
-        //}).done((function (a) {
-            //var b = {
-                //id: a[0].items[0].itemId,
-                //quantity: 1,
-                //seller: "1"
-            //};
-        //}))
-    //}))
-//}
-
-// Em testes
 function fnPdpAddtoCart() {
     $("body").on("click", ".addCart_product", (function (a) {
         a.preventDefault();
