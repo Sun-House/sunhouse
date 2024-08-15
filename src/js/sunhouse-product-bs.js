7f39deb4dd9f3c3ab72982d01d05dc647e20f6d6
AlexJSant <alex.jose7764@gmail.com>
2024-08-15 12:30:00 -0300

    Atualizados:     sunhouse-product-bs.css     sunhouse-product-bs.js

--- src/js/sunhouse-product-bs.js
+++ src/js/sunhouse-product-bs.js
@@ -427,13 +427,13 @@ $(document).ready(function () {
         });
         
         //$(".productContent__options").click(function () {
-        $(".productContent__options-opinions").click(function () {
+        //$(".productContent__options-opinions").click(function () {
             // Abre o accordion de avaliações
-            $("#rate-accordion").collapse("show");
+            //$("#rate-accordion").collapse("show");
             // Rola a tela até a posição do accordion
             //scrollToElement(".ts-button--more");
-            scrollToElement(".ts-customer-questions");
-        });
+            //scrollToElement(".ts-customer-questions");
+        //});
     } else {
         // Botão para a descrição do produto
         $("#btnScrollSt").click(function () {
@@ -452,13 +452,13 @@ $(document).ready(function () {
             scrollToElement("#rate-tab");
         });
 
-        $(".productContent__options-opinions").click(function () {
+        //$(".productContent__options-opinions").click(function () {
             // Abre o accordion de avaliações
-            $("#rate-accordion").collapse("show");
+            //$("#rate-accordion").collapse("show");
             // Rola a tela até a posição do accordion
             //scrollToElement(".ts-button--more");
-            scrollToElement(".ts-customer-questions");
-        });
+            //scrollToElement(".ts-customer-questions");
+        //});
     }
 });
 
